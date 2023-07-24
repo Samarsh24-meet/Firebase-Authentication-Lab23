5953facf1627f0c040a5541cf1578afb760f2f58
Samarsh24-meet <139688951+Samarsh24-meet@users.noreply.github.com>
2023-07-24 10:29:11 +0300

    still working on it

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -10,11 +10,12 @@ firebaseConfig = {
   "messagingSenderId": "920213704699",
   "appId": "1:920213704699:web:eb7b00d5d8c2de67d0055e",
   "measurementId": "G-HMPF6VNE6L",
-  "databaseURL": ""
+  "databaseURL": "https://samar-s-project-default-rtdb.europe-west1.firebasedatabase.app/"
 };
 
 firebase = pyrebase.initialize_app(firebaseConfig)
 auth = firebase.auth()
+db = firebase.database()
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
@@ -41,6 +42,9 @@ def signup():
             email = request.form['email']
             password = request.form['password']
             login_session['user'] = auth.create_user_with_email_and_password(email,password)
+            UID = login_session['users']['localId']
+            user = {'fullname':request.form['fullname'],'username':request.form['username'],'bio':request.form['bio']}
+            db.child("users").child(UID).set(user)
             return redirect(url_for("add_tweet"))
     except:
         return redirect(url_for('signup'))
