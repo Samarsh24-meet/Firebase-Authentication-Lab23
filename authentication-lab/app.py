37865034d83e08d3fb7d4c495f38d45b52b1a355
Samarsh24-meet <139688951+Samarsh24-meet@users.noreply.github.com>
2023-07-24 14:13:13 +0300

    still  workingon it

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -27,7 +27,7 @@ def signin():
         if request.method == 'POST':
             email = request.form['email']
             password = request.form['password']
-            login_session['user'] = auth.sign_in_with_email_and_password(email,password)
+            login_session['users'] = auth.sign_in_with_email_and_password(email,password)
             return  redirect(url_for('add_tweet'))
     except:
         return redirect(url_for("signin"))
@@ -37,24 +37,38 @@ def signin():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    try:
-        if request.method == 'POST':
+    if request.method == 'POST':
+        try:
             email = request.form['email']
             password = request.form['password']
-            login_session['user'] = auth.create_user_with_email_and_password(email,password)
+            login_session['users'] = auth.create_user_with_email_and_password(email,password)
             UID = login_session['users']['localId']
-            user = {'fullname':request.form['fullname'],'username':request.form['username'],'bio':request.form['bio']}
+            user = {'fullname':request.form['full_name'],'username':request.form['username'],'bio':request.form['bio']}
             db.child("users").child(UID).set(user)
             return redirect(url_for("add_tweet"))
-    except:
-        return redirect(url_for('signup'))
+        except Exception as e:
+            print(e)
+            return redirect(url_for('signup'))
     return render_template("signup.html")
 
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
+    if request.method=='POST':
+        try:
+            UID = login_session['users']['localId']
+            tweet = {'title':request.form['title'], 'text': request.form['text'], 'UID': UID}
+            db.child("users").child(UID).push(tweet)
+            db.child("tweets").child(UID).set(tweet)
+            return render_template("add_tweet.html")
+        except:
+            return render_template("signup.html")
     return render_template("add_tweet.html")
-
+@app.route('/all_tweets')
+def all_tweets():
+    UID = login_session['users']['localId']
+    AT = db.child(UID).child().get().val()
+    return render_template("tweets.html", at = AT)
 
 if __name__ == '__main__':
     app.run(debug=True)
